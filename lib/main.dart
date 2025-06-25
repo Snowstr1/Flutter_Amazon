a2fc104e756fda14d3e93d70f425a7a91a6dbd1a
Amarbat <amarbat1001@gmail.com>
2025-06-25 14:44:10 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Amazon Project',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const Scaffold(body: Center(child: Text('Amazon Project'))),
+    );
+  }
+}
