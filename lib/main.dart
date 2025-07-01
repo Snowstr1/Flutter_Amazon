7f7f16e92f9e459f94fddc15d60b218d9c5387d1
Amarbat <amarbat1001@gmail.com>
2025-07-01 09:24:34 +0800

    Project New

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:amazon_new/constants/global_variables.dart';
+import 'package:amazon_new/features/auth/home/screens/home_screen.dart';
+import 'package:amazon_new/features/auth/screens/auth_screen.dart';
+import 'package:amazon_new/features/auth/services/auth_service.dart';
+import 'package:amazon_new/providers/user_provider.dart';
+import 'package:amazon_new/router.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [ChangeNotifierProvider(create: (context) => UserProvider())],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final AuthService authService = AuthService();
+  @override
+  void initState() {
+    super.initState();
+    authService.getUserData(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Amazon Project',
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black),
+        ),
+      ),
+      onGenerateRoute: generateRoute,
+      home: Provider.of<UserProvider>(context).user.token.isNotEmpty
+          ? const HomeScreen()
+          : const AuthScreen(),
+    );
+  }
+}
